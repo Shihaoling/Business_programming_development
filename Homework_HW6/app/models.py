d9091cb9fd0543ae20d23916bff9790deada5f07
Shihaoling <henryhlshi@gmail.com>
2023-06-11 19:57:39 +0800

    General

---
+++ Homework_HW6/app/models.py
@@ -0,0 +1,38 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+from app import db
+
+class Suppliers(db.Model):
+    SupplierID = db.Column(db.Integer, primary_key=True)
+    CompanyName = db.Column(db.String(40), nullable=False)
+    City = db.Column(db.String(15), nullable=True)
+
+# class Stats(db.Model):
+
+#     id          = db.Column(db.Integer,   primary_key=True )
+#     month       = db.Column(db.String(64),    unique=True  )
+#     sold_units  = db.Column(db.Integer                     )
+#     total_sales = db.Column(db.Integer                     )
+
+#     def __init__(self, id, month, sold_units, total_sales):
+#         self.id          = id
+#         self.month       = month
+#         self.sold_units  = sold_units
+#         self.total_sales = total_sales
+
+#     def __repr__(self):
+#         return self.month + ' = ' + str(self.sold_units) + '/ ' + str(self.total_sales)
+
+#     # Optional helper
+#     def save(self):
+
+#         # inject self into db session    
+#         db.session.add ( self )
+
+#         # commit change and save the object
+#         db.session.commit( )
+
+#         return self 
---
+++ Homework_HW6/app/views.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+"""
+Copyright (c) 2019 - present AppSeed.us
+"""
+
+# Flask modules
+from flask   import render_template, request, redirect, url_for, flash
+from jinja2  import TemplateNotFound
+
+# App modules
+from app import app, db
+from app.models import Suppliers
+
+# App main route + generic routing
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/supplier')
+def supplier():
+    return render_template('supplier.html')
+
+@app.route('/form')
+def form():
+    return render_template('form.html')
